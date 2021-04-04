cc1d59af10197d36b85347130c37f516fce2c0e9
LeUnicorn <denisa@graffino.com>
2021-04-04 14:39:14 +0300

    rename file

---
+++ simple_menu.asm
@@ -0,0 +1,92 @@
+.model small
+.stack 
+
+.data 
+
+element1 db 'F', 1EH, 'I', 1EH, 'L', 1EH, 'E', 1EH
+element2 db 'E', 1EH, 'D', 1EH, 'I', 1EH, 'T', 1EH
+element3 db 'E', 1EH, 'X', 1EH, 'I', 1EH, 'T', 1EH
+
+element1_active db 'F', 3EH, 'I', 3EH, 'L', 3EH, 'E', 3EH
+element2_active db 'E', 3EH, 'D', 3EH, 'I', 3EH, 'T', 3EH
+element3_active db 'E', 3EH, 'X', 3EH, 'I', 3EH, 'T', 3EH
+current_element db 0
+
+.code
+start:
+  mov ax,@data
+  mov ds, ax
+  
+  mov ax,0B800H 
+  mov ES,ax 
+    
+  mov di,0 
+  lea si,element1 
+  mov cx,8 
+  rep movsb
+  
+  mov di,160 
+  lea si,element2 
+  mov cx,8 
+  rep movsb
+
+  mov di,320 
+  lea si,element3 
+  mov cx,8
+  rep movsb
+  
+poll_for_input:
+  mov dl,255
+  mov ah,06h              
+  int 21h                 
+  jnz handle_input
+  jmp poll_for_input
+    
+handle_input:
+  cmp al,"s"
+  jz keydown
+  cmp al,'w'
+  jz keyup
+  jmp keyup
+  
+keydown:
+  call cs:increment_counter
+  call cs:rerender_cursor
+  jmp poll_for_input
+  
+keyup:
+  call cs:decrement_counter
+  call cs:rerender_cursor
+  jmp poll_for_input
+
+rerender_cursor proc far
+  mov ah,02h
+  mov dh,current_element
+  mov dl,0
+  int 10h
+  ret 
+rerender_cursor endp
+
+increment_counter proc far
+  cmp current_element,2
+  jz reset_counter_to_start
+  inc current_element
+  jmp fin2
+reset_counter_to_start:
+  mov current_element,0
+fin2:
+  ret
+increment_counter endp
+
+decrement_counter proc far
+  cmp current_element,0
+  jz reset_counter_to_end
+  dec current_element
+  jmp fin
+reset_counter_to_end:
+  mov current_element,2
+fin:
+   ret
+decrement_counter endp
+  
+end start
